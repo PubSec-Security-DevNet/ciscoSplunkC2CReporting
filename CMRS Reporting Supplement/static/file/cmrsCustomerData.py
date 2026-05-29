419678d905261af73f40ebcbd11ad33764e2a702
chadmi <64537108+chadmi@users.noreply.github.com>
2026-05-29 09:49:06 -0400

    Updated API Connectivity Defaults and Comments

--- CMRS Reporting Supplement/static/file/cmrsCustomerData.py
+++ CMRS Reporting Supplement/static/file/cmrsCustomerData.py
@@ -34,10 +34,10 @@ reportingGeolocation = "12345"
 # API Connectivity and Security 
 # =========================================
 # Update the following to match the Endpoint and certificate data provided during your registration with CMRS
-soapEndpoint = "https://adslite.dev.cmrs.com:5000"    # DISA CMRS SOAP Endpoint
+soapEndpoint = "https://adslite.dev.cmrs.com"    # DISA CMRS SOAP Endpoint
 certFile = "client.crt"             # Path to your client certificate (PEM)
 keyFile = "client.key"              # Path to your private key (PEM)
-pkiTrust = "cmrsProvidedTrust.crt"  # Trusted CMRS certificate to verify the server
+pkiTrust = "cmrsProvidedTrust.crt"  # Trusted CMRS certificate to verify the server. This should be a single file with the full chain (PEM encoded) of trust to the ADSLITE endpoint
 # Max retries for failed submissions when not in debug mode
 cmrs_max_retries = 3
 
